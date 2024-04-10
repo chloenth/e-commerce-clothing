9e364442b07da0eb96f19b2aab779c21a10777a6
Huong <huongnguyen.nth1709@gmail.com>
2024-04-10 22:17:08 +0300

    clean up code

--- src/routes/authentication/Authentication.jsx
+++ src/routes/authentication/Authentication.jsx
@@ -1,25 +1,12 @@
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-} from '../../utils/firebase/FirebaseUtils';
 import SignUpForm from '../../components/sign-up-form/SignUpForm';
-import Button from '../../components/button/Button';
 import SignInForm from '../../components/sign-in-form/SignInForm';
+import './styles.scss';
 
 const Authentication = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
-  };
-
   return (
-    <div>
-      <h1>Sign In Page</h1>
+    <div className='authentication-container'>
       <SignInForm />
       <SignUpForm />
-      <Button buttonType='google' onClick={logGoogleUser}>
-        Sign in with Google Popup
-      </Button>
     </div>
   );
 };
