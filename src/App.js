a980fa64c1ed7a535963ed9439a9fb8478aa4efe
Huong <huongnguyen.nth1709@gmail.com>
2024-04-10 11:07:20 +0300

    re-structure and add Directory component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './categories.styles.scss';
+import Directory from './components/directory/Directory';
 
 function App() {
   const categories = [
@@ -30,20 +31,9 @@ function App() {
   ];
 
   return (
-    <div className='categories-container'>
-      {categories.map(({ title, id, imageUrl }) => (
-        <div key={id} className='category-container'>
-          <div
-            className='background-image'
-            style={{ backgroundImage: `url(${imageUrl})` }}
-          />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <>
+      <Directory categories={categories} />
+    </>
   );
 }
 
