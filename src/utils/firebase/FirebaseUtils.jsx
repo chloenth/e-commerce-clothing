9191d7ed5bb94d1e4f67a99303f1bcf289b6f00e
Huong <huongnguyen.nth1709@gmail.com>
2024-04-10 16:59:57 +0300

    create connection to firestore and create userDocRef in users collection

--- src/utils/firebase/FirebaseUtils.jsx
+++ src/utils/firebase/FirebaseUtils.jsx
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBTTEiDvUKfsT--UJZYUAKcXOehyDzBNSE',
@@ -26,3 +27,31 @@ provider.setCustomParameters({
 
 export const auth = getAuth(firebaseApp);
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore(firebaseApp);
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  // if user data does not exist
+  // create / set the document with the data from userAuth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  // if user data exist
+  // return userDocRef
+  return userDocRef;
+};
