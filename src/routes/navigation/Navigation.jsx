1b17f54347dffbeed0e6dca38ac4c92ef2e3461a
Huong <huongnguyen.nth1709@gmail.com>
2024-04-10 15:15:36 +0300

    add logo and nav links

--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -1,11 +1,24 @@
-import { Outlet } from 'react-router-dom';
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './styles.scss';
 
 const Navigation = () => {
   return (
-    <div>
-      <div>I am the Navigation Bar</div>
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+        </div>
+      </div>
       <Outlet />
-    </div>
+    </Fragment>
   );
 };
 
