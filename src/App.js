091cbb206b35bf2a77e6fd5ed432169417b30abe
Huong <huongnguyen.nth1709@gmail.com>
2024-04-10 15:16:15 +0300

    add route signin to App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/Home';
 import Shop from './routes/shop/Shop';
 import Navigation from './routes/navigation/Navigation';
+import Signin from './routes/signin/Signin';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='signin' element={<Signin />} />
       </Route>
     </Routes>
   );
