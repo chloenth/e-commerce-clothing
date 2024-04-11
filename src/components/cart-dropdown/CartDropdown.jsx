af95f2ac4035027fc481fa860f00441e84b249b5
Huong <huongnguyen.nth1709@gmail.com>
2024-04-11 14:14:33 +0300

    adding CartContext and implement logic of adding cart item into cart dropdown as well as increment quantity if it already exist

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,10 +1,20 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/CartContext';
 import Button from '../button/Button';
+import CartItem from '../cart-item/CartItem';
 import './styles.scss';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className='cart-dropdown-container'>
-      <div>Cart Item</div>
+      <div className='cart-items'>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
