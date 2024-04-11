c9f0d15e50a7181bb2f668c8da20b251e7d8fe5d
Huong <huongnguyen.nth1709@gmail.com>
2024-04-11 20:22:32 +0300

    fix clashing styles

--- src/routes/category/Category.jsx
+++ src/routes/category/Category.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { CategoriesContext } from '../../contexts/CategoriesContext';
+import ProductCard from '../../components/product-card/ProductCard';
+
+import './styles.scss';
 
 const Category = () => {
-  return <div>Category</div>;
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <div className='category-container'>
+      {products &&
+        products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+    </div>
+  );
 };
 
 export default Category;
