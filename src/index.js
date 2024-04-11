4163d8ede24823b6e48c590092bb9e94dc76f896
Huong <huongnguyen.nth1709@gmail.com>
2024-04-11 09:34:46 +0300

    add UserProvider as a container of the App

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { UserProvider } from './contexts/UserContext';
 
 const router = createBrowserRouter([
   {
@@ -15,7 +16,9 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
   </React.StrictMode>
 );
 
