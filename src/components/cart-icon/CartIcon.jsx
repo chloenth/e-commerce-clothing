6ea8fa5cf613bc0ff0477a3606f47eb52277938e
Huong <huongnguyen.nth1709@gmail.com>
2024-04-12 12:06:35 +0300

    convert scss styles into styled-component of Directory Item and Cart Icon

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -1,18 +1,18 @@
 import { useContext } from 'react';
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
 import { CartContext } from '../../contexts/CartContext';
 
-import './styles.scss';
+import { ShoppingIcon, CartIconContainer, ItemCount } from './cart-icon.styles';
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
-    <div className='cart-icon-container'>
+    <CartIconContainer>
       <ShoppingIcon className='shopping-icon' onClick={toggleIsCartOpen} />
-      <span className='item-count'>{cartCount}</span>
-    </div>
+      <ItemCount>{cartCount}</ItemCount>
+    </CartIconContainer>
   );
 };
 
